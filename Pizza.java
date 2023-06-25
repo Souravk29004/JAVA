9130c7f8118348578592ba852e9da4f001ed80f8
Souravk29004 <88694528+Souravk29004@users.noreply.github.com>
2023-06-25 12:22:44 +0530

    Java

---
+++ Pizza.java
@@ -0,0 +1,20 @@
+public class Pizza {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("I live Pizza");
+		System.out.println("My favourite Pizza is Pepperoni");
+
+		// This is a comment
+		
+		/*
+		 * This
+		 * is
+		 * a
+		 * comment
+		 */
+		
+	}
+
+}
---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+	public static void main(String[] args){
+		
+		int x = 123;
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Pizza";
+		
+		System.out.println("I want "+ name);
+		
+	}
+}
