d2afa812c4c397af278ac63323a6becabc3450a1
Souravk29004 <souravk29004@gmail.com>
2023-06-26 07:12:35 +0530

    Java3

---
+++ Expressions.java
@@ -0,0 +1,18 @@
+public class Expressions {
+
+ public static void main(String[] args) {
+  
+  // expression =     operands & operators
+  // operands =  values, variables, numbers, quantity
+  // operators = + - * / % 
+
+	 double pizza = 10;
+
+	 pizza = (double) pizza /3;
+
+	 pizza++;
+
+	 System.out.println(pizza + " Pizzas");
+
+ } 
+}
